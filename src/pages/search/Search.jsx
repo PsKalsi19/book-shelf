4fa1c43c7b3d519b0dc9ee9dc0ee8d16f5873fd8
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-06-17 22:23:28 +0530

    added margin

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
 
                 { 
            searchText!=='' &&
-           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4">
+           <div className="grid grid-cols-2 gap-4 mt-8 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4">
                 {searchByText().map(product => <Cards key={product.id} book={product} />)}
             </div>
             }
